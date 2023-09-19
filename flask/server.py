c0b28b8571852be70a872f9e747b4534545da5fb
Ali Tajir <alitajir32@gmail.com>
2023-09-19 14:22:09 +0500

    added subtraction function to server.py

--- flask/server.py
+++ flask/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({'CA':'3', 'Member 1':'Muhammad Shehzad (20I-1756)'})
+    return jsonify({'CA':'3', 'Member 1':'Muhammad Shehzad (20I-1756)','Member 2':'Ali Tajir (20I-0512)'})
 
 
 @app.route('/cal/add/<num1>/<num2>')
@@ -16,6 +16,13 @@ def add(num1, num2):
     except ValueError:
         return jsonify({"error": "Invalid input. Please provide valid numbers."})
 
-
+@app.route('/cal/sub/<num1>/<num2>')
+def sub(num1, num2):
+    try:
+        result = int(num1) - int(num2)
+        return jsonify({"result": result})
+    except ValueError:
+        return jsonify({"error": "Invalid input. Please provide valid numbers."})
+    
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
