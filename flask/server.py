7981b27cc26981b978ddce801bf8d0b45f21495e
Zahid <i200469@nu.edu.pk>
2023-09-19 14:17:29 +0500

    div and mul function added (i200469)

--- flask/server.py
+++ flask/server.py
@@ -16,6 +16,22 @@ def add(num1, num2):
     except ValueError:
         return jsonify({"error": "Invalid input. Please provide valid numbers."})
 
+@app.route('/cal/div/<num1>/<num2>')
+def div(num1, num2):
+    try:
+        result = int(num1) // int(num2)
+        return jsonify({"result": result})
+    except ValueError:
+        return jsonify({"error": "Invalid input. Please provide valid numbers."})
+
+@app.route('/cal/mul/<num1>/<num2>')
+def mul(num1, num2):
+    try:
+        result = int(num1) * int(num2)
+        return jsonify({"result": result})
+    except ValueError:
+        return jsonify({"error": "Invalid input. Please provide valid numbers."})
+
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
